04ee1d7c21a270202b0f9570c707902dee6576a0
atm-florian <florian.mortgat@atm-consulting.fr>
2022-09-14 13:48:00 +0200

    ajouté fonction filtre_vanilla (équivalente à filtre mais sans jQuery)

--- solultion.js
+++ solultion.js
@@ -1,3 +1,17 @@
+function filtre_vanilla() {
+    let filterCateg = document.querySelector('#filter-categ').value;
+    let mapping = {
+        'avion': 'av',
+        'bateau': 'bat',
+        'vélo': undefined, // pour plus tard?
+    };
+    let classShown = mapping[filterCateg];
+    document.querySelectorAll('tbody > tr').forEach((tr)=>{
+        tr.style.display = 'none';
+        if (tr.classList.contains(classShown)) tr.style.display = 'table-row';
+    });
+
+}
 
 function filtre() {
 
@@ -15,9 +29,9 @@ function filtre() {
             $(".bat").hide()
         }
         else {
-            $(".av").show()           
+            $(".av").show()
              $(".bat").show()
 
-        }   
+        }
 
      }
